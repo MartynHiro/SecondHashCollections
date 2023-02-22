7cf646bee20507e421db28d7dda7bd0a0619b811
Сергей <123937500+MartynHiro@users.noreply.github.com>
2023-02-22 19:54:56 +0700

    Update WordsChecker.java
    Just a little more changes in WordsChecker class

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,11 +3,9 @@ import java.util.List;
 import java.util.Set;
 
 public class WordsChecker {
-    protected String text;
     protected Set<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
-        this.text = text;
         this.set.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
     }
 
