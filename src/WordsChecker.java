feabeaeaec47f3eab133109b10676ee7f8479ab4
Сергей <123937500+MartynHiro@users.noreply.github.com>
2023-02-22 19:57:32 +0700

    Update WordsChecker.java

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -5,11 +5,7 @@ import java.util.Set;
 public class WordsChecker {
     protected Set<String> set = new HashSet<>();
 
-    public WordsChecker(String text) {
-        this.set.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
-    }
+    public WordsChecker(String text) { this.set.addAll(List.of(text.split("\\P{IsAlphabetic}+"))); }
 
-    public boolean hasWord(String searchingWord) {
-        return set.contains(searchingWord);
-    }
+    public boolean hasWord(String searchingWord) { return set.contains(searchingWord); }
 }
